1ed47f3bec739675a2cb364a8cf7530d4cf7f600
CelloRen <939549708@qq.com>
2021-08-09 11:53:03 +0800

    需求变更，老板要换数据库，需要改dao的代码

--- src/main/java/com/cello/serivce/impl/HelloServiceImpl.java
+++ src/main/java/com/cello/serivce/impl/HelloServiceImpl.java
@@ -1,13 +1,13 @@
 package com.cello.serivce.impl;
 
 import com.cello.dao.HelloDao;
-import com.cello.dao.impl.HelloDaoImpl;
+import com.cello.dao.impl.HelloDaoImpl2;
 import com.cello.serivce.HelloService;
 
 import java.util.List;
 
 public class HelloServiceImpl implements HelloService {
-    private HelloDao helloDao = new HelloDaoImpl();
+    private HelloDao helloDao = new HelloDaoImpl2();
 
     @Override
     public List<String> findAll() {
