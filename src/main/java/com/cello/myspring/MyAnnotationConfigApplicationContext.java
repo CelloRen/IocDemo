72fe6658bf554857b0c01105b508b37bfa559506
CelloRen <939549708@qq.com>
2021-08-09 20:32:58 +0800

    根据原材料创建对象，放入缓存

--- src/main/java/com/cello/myspring/MyAnnotationConfigApplicationContext.java
+++ src/main/java/com/cello/myspring/MyAnnotationConfigApplicationContext.java
@@ -1,12 +1,23 @@
 package com.cello.myspring;
 
+import com.cello.spring.entity.Account;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class MyAnnotationConfigApplicationContext {
+    private Map<String, Object> iocMap = new HashMap<>();
+
     public MyAnnotationConfigApplicationContext(String packageName) {
+        // 获取原材料：beanName和对应的class
         Set<MyBeanDefinition> beanDefinitions = findBeanDefinitions(packageName);
-        beanDefinitions.forEach(bean -> System.out.println(bean));
+        // 根据原材料创建对象, 并存入缓存
+        createObject(beanDefinitions);
     }
 
     // 获取原材料
@@ -32,7 +43,60 @@ public class MyAnnotationConfigApplicationContext {
         return beanDefinitionSet;
     }
 
+    public Object getBean(String beanName) {
+        return iocMap.get(beanName);
+    }
+
+    public void createObject(Set<MyBeanDefinition> beanDefinitionSet) {
+        beanDefinitionSet.forEach(beanDefinition -> {
+            Class clazz = beanDefinition.getBeanClass();
+            try {
+                // 根据构造器创建对象
+                Object object = clazz.getConstructor().newInstance();
+
+                // 属性赋值
+                for (Field declaredField : clazz.getDeclaredFields()) {
+                    MyValue myValueAnnotation = declaredField.getAnnotation(MyValue.class);
+                    if (myValueAnnotation != null) {
+                        String value = myValueAnnotation.value();
+                        String filedName = declaredField.getName();
+                        String methodName = "set" + filedName.substring(0, 1).toUpperCase() + filedName.substring(1);
+                        Method method = clazz.getMethod(methodName, declaredField.getType());
+                        // 数据类型转化
+                        Object val = convertValue(value, declaredField.getType().getName());
+                        method.invoke(object, val);
+                    }
+                }
+                // 存入缓存
+                iocMap.put(beanDefinition.getBeanName(), object);
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private Object convertValue(String value, String typeName) {
+        switch (typeName) {
+            case "java.lang.Integer":
+                return Integer.valueOf(value);
+            case "java.lang.String":
+                return value;
+            case "java.lang.Float":
+                return Float.valueOf(value);
+            // and so on
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         MyAnnotationConfigApplicationContext test = new MyAnnotationConfigApplicationContext("com.cello.spring");
+        Account account = (Account) test.getBean("myAccount");
+        System.out.println(account);
     }
 }
